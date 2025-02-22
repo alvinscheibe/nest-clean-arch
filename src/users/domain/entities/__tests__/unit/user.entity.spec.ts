e2cc5806b50826ad92c77fa272e33de1ab5bbc9f
Alvin Scheibe <alvin.scheibe@gmail.com>
2025-02-22 09:48:10 -0300

    Add testes to user entity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -20,4 +20,27 @@ describe('UserEntity unit test', () => {
     expect(sut.props).toStrictEqual(props);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('should get of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('should get of email field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('should get of password field', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('should get of createdAt field', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
 });
